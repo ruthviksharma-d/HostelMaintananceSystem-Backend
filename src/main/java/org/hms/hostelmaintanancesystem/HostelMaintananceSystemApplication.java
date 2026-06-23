6aa3027ba9c66320eba0e11a6d39ff2371558919
Ashwanth Reddy Boddireddy <ashwanthreddy@Ashwanths-MacBook-Air.local>
2026-06-23 16:08:10 +0530

    Initial setup with added dependencies

---
+++ src/main/java/org/hms/hostelmaintanancesystem/HostelMaintananceSystemApplication.java
@@ -0,0 +1,13 @@
+package org.hms.hostelmaintanancesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelMaintananceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelMaintananceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hms/hostelmaintanancesystem/HostelMaintananceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hms.hostelmaintanancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelMaintananceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
